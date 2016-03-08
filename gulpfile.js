c7e54409f5d4b8fbb750aa7fbd364c31ab76ba0a
flyImt <839126257@qq.com>
2016-03-09 07:27:40 +0800

    * optimize：optimize the structure of js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,30 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('css', function() {
   gulp.src('css/flyimt.css')
     .pipe(gulp.dest('dist/css/'));
 })
 
-gulp.task('default', ['css']);
+gulp.task('sourcemap', function() {
+  gulp.src('js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write());
+})
+
+gulp.task('concatjs', function() {
+  gulp.src('js/*.js')
+    .pipe(concat('flyimt.js'))
+    .pipe(gulp.dest('dist/js/'))
+})
+
+gulp.task('uglifyjs', function() {
+  gulp.src('js/*.js')
+    .pipe(concat('flyimt.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/'))
+})
+
+gulp.task('default', ['css','sourcemap','concatjs','uglifyjs']);
