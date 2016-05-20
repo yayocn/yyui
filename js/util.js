68fbe68e36a9ddd53387b2dbf643dce5707afc8d
yayo <839126257@qq.com>
2016-05-20 08:44:05 +0800

    * add： carousel

--- js/util.js
+++ js/util.js
@@ -9,6 +9,71 @@
 
 var Util = (function ($) {
 
+  var transition = false;
+
+  var TransitionEndEvent = {
+    WebkitTransition: 'webkitTransitionEnd',
+    MozTransition: 'transitionend',
+    OTransition: 'oTransitionEnd otransitionend',
+    transition: 'transitionend'
+  };
+
+  function getSpecialTransitionEndEvent() {
+    return {
+      bindType: transition.end,
+      delegateType: transition.end,
+      handle: function handle(event) {
+        if ($(event.target).is(this)) {
+          return event.handleObj.handler.apply(this, arguments);
+        }
+      }
+    };
+  }
+
+  function transitionEndTest() {
+    if (window.QUnit) {
+      return false;
+    }
+
+    var el = document.createElement('yyui');
+
+    for (var _name in TransitionEndEvent) {
+      if (el.style[_name] !== undefined) {
+        return { end: TransitionEndEvent[_name] };
+      }
+    }
+
+    return false;
+  }
+
+  function transitionEndEmulator(duration) {
+    var _this = this;
+
+    var called = false;
+
+    $(this).one(Util.TRANSITION_END, function () {
+      called = true;
+    });
+
+    setTimeout(function () {
+      if (!called) {
+        Util.triggerTransitionEnd(_this);
+      }
+    }, duration);
+
+    return this;
+  }
+
+  function setTransitionEndSupport() {
+    transition = transitionEndTest();
+
+    $.fn.emulateTransitionEnd = transitionEndEmulator;
+
+    if (Util.supportsTransitionEnd()) {
+      $.event.special[Util.TRANSITION_END] = getSpecialTransitionEndEvent();
+    }
+  }
+
   /**
    * --------------------------------------------------------------------------
    * Public Util Api
@@ -16,6 +81,8 @@ var Util = (function ($) {
    */
 
   var Util = {
+    TRANSITION_END: 'transitionEnd',
+
     getSelectorFromElement: function getSelectorFromElement(element) {
       var selector = element.getAttribute('data-target');
 
@@ -26,6 +93,7 @@ var Util = (function ($) {
 
       return selector;
     },
+
     setAnimateToggleForTarget: function setAnimateToggleForTarget(target, type, duration, callback) {
       type = type || 'slide';
       duration = duration || 'fast';
@@ -39,6 +107,7 @@ var Util = (function ($) {
           break;
       }
     },
+
     setAnimateShowForTarget: function setAnimateShowForTarget(target, type, duration, callback) {
       type = type || 'slide';
       duration = duration || 'fast';
@@ -52,6 +121,7 @@ var Util = (function ($) {
           break;
       }
     },
+
     setAnimateHideForTarget: function setAnimateHideForTarget(target, type, duration, callback) {
       type = type || 'slide';
       duration = duration || 'fast';
@@ -64,8 +134,21 @@ var Util = (function ($) {
           $(target).fadeOut(duration, callback);
           break;
       }
-    }
+    },
+
+    reflow: function reflow(element) {
+      new Function('yyui', 'return yyui')(element.offsetHeight);
+    },
+
+    triggerTransitionEnd: function triggerTransitionEnd(element) {
+      $(element).trigger(transition.end);
+    },
+
+    supportsTransitionEnd: function supportsTransitionEnd() {
+      return Boolean(transition);
+    },
   };
 
+  setTransitionEndSupport();
   return Util;
 })(jQuery);
